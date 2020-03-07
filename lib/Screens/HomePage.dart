6349e451f6674f677a80e3c1bbe3734fb93de92c
SAMBIT MAJHI <majhisambit2@gmail.com>
2020-03-07 18:18:56 +0530

    upload

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -16,36 +16,41 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            RaisedButton(
+            FlatButton(
+              color: Colors.grey[300],
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              padding: EdgeInsets.fromLTRB(80, 20, 80, 20),
               onPressed: () {
                 Navigator.pushNamed(context, '/PoMang');
               },
-              child: Text('Po Mangement'),
-
+              child: Text('Po Mangement', style: TextStyle(fontSize: 25.0)),
             ),
-            RaisedButton(
+            FlatButton(
+              color: Colors.grey[300],
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
               onPressed: () {
                 Navigator.pushNamed(context, '/MerchandisingMang');
               },
-              child: Text('Merchandising Mangement'),
+              child: Text('Merchandising Mangement', style: TextStyle(fontSize: 25.0)),
             ),
-            RaisedButton(
+            FlatButton(
+              color: Colors.grey[300],
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              padding: EdgeInsets.fromLTRB(30, 20, 30, 20),
               onPressed: () {
                 Navigator.pushNamed(context, '/ProductionMang');
               },
-              child: Text('Production Mangement'),
+              child: Text('Production Mangement', style: TextStyle(fontSize: 25.0)),
             ),
-            RaisedButton(
+            FlatButton(
+              color: Colors.grey[300],
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              padding: EdgeInsets.fromLTRB(60, 20, 60, 20),
               onPressed: () {
                 Navigator.pushNamed(context, '/QualityMang');
               },
-              child: Text('Quality Mangement'),
-            ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/DocumentationMang');
-              },
-              child: Text('Documentation Mangement'),
+              child: Text('Quality Mangement', style: TextStyle(fontSize: 25.0)),
             ),
           ],
         ),
