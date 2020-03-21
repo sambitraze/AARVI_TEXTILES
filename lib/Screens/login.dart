1a94cfe40904a88d16e052a8eb6c5b87c0b34f69
SAMBIT MAJHI <majhisambit2@gmail.com>
2020-03-22 01:48:59 +0530

    update and upload

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -1,6 +1,8 @@
 import 'package:aarvi_textiles/services/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:aarvi_textiles/widget/bezierContainer.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Loginscreen extends StatefulWidget {
   @override
@@ -18,91 +20,206 @@ class _LoginscreenState extends State<Loginscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Center(child: Text('AARVI TEXTILES')), backgroundColor: Colors.brown,),
-      body:  Center(
-      child: SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.symmetric(vertical: 50,horizontal: 20),
-          child: Form(
-            key: _formKey,
-            child: Column(
+            height: MediaQuery.of(context).size.height,
+            child: Stack(
               children: <Widget>[
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    hintText: "Enter Email",
-                  ),
-                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  },
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                TextFormField(
-                  autocorrect: true,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: "Enter Password",
-                  ),
-                  validator: (val) => val.isEmpty ? 'Enter password' : null,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  },
-                ),
-                SizedBox(
-                  height: 35,
-                ),
-                RaisedButton(
-                  child: Text("Sign-In"),
-                  onPressed: () async {
-                    setState(() {
-                      _loggingIn = true;
-                    });
-                    FirebaseUser user = await _auth.signIn(email, password);
-                    setState(() {
-                      _loggingIn = false;
-                      error = 'Invalid Username and Password!';
-                    });
-                    if(user.uid == null){
-                      user = null;
-                      setState(() {
-                        error = "Could not log in";
-                      });
-                    }
-                    else{
-                      print(user.uid);
-                      print(user.email);
-                      setState(() {
-                        error = "";
-                      });
-                    }
-                  },
-                ),
-                RaisedButton(
-                  child: Text("Sign in (ONLY FOR DEBUG)"),
-                  onPressed: () async {
-                    _loggingIn = true;
-                    AuthResult result = await FirebaseAuth.instance.signInAnonymously();
-                    print(result.user.uid);
-                    _loggingIn = false;
-                    Navigator.pushNamed(context, '/AdminHomePage');
-                  },
-                ),
-
+                Positioned(
+                    top: -MediaQuery.of(context).size.height * .15,
+                    right: -MediaQuery.of(context).size.width * .4,
+                    child: BezierContainer()),
                 Container(
-                  child: !_loggingIn? Text(error,
-                  style: TextStyle(
-                    color: Colors.red
-                  ),): CircularProgressIndicator(),
-                )
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 3,
+                          child: SizedBox(),
+                        ),
+                        RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                              text: 'A',
+                              style: GoogleFonts.portLligatSans(
+                                textStyle: Theme.of(context).textTheme.display1,
+                                fontSize: 50,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.cyan,
+                              ),
+                              children: [
+                                TextSpan(
+                                  text: 'AR',
+                                  style: TextStyle(
+                                      color: Colors.blue, fontSize: 50),
+                                ),
+                                TextSpan(
+                                  text: 'VI',
+                                  style: TextStyle(
+                                      color: Colors.greenAccent, fontSize: 50),
+                                ),
+                                TextSpan(
+                                  text: '  Textiles',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 50),
+                                ),
+                              ]),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            hintText: "Enter Email",
+                          ),
+                          validator: (val) =>
+                              val.isEmpty ? 'Enter an email' : null,
+                          onChanged: (val) {
+                            setState(() => email = val);
+                          },
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          autocorrect: true,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            hintText: "Enter Password",
+                          ),
+                          validator: (val) =>
+                              val.isEmpty ? 'Enter password' : null,
+                          onChanged: (val) {
+                            setState(() => password = val);
+                          },
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        GestureDetector(
+                          onTap: () async{
+                            setState(() {
+                              _loggingIn = true;
+                            });
+                            FirebaseUser user =
+                                await _auth.signIn(email, password);
+                            setState(() {
+                              _loggingIn = false;
+                              error = 'Invalid Username and Password!';
+                            });
+                            if (user.uid == null) {
+                              user = null;
+                              setState(() {
+                                error = "Could not log in";
+                              });
+                            } else {
+                              print(user.uid);
+                              print(user.email);
+                              setState(() {
+                                error = "";
+                              });
+                            }
+                          },
+                          child: Container(
+                            width: 150,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Colors.blueAccent,
+                                  Colors.greenAccent,
+                                ],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              ),
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  offset: Offset(5, 5),
+                                  blurRadius: 10,
+                                )
+                              ],
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Sign In',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10.0),
+                        GestureDetector(
+                          onTap: () async {
+                            _loggingIn = true;
+                            AuthResult result =
+                                await FirebaseAuth.instance.signInAnonymously();
+                            print(result.user.uid);
+                            _loggingIn = false;
+                            Navigator.pushNamed(context, '/AdminHomePage');
+                          },
+                          child: Container(
+                            width: 300,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Colors.blueAccent,
+                                  Colors.greenAccent,
+                                ],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              ),
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black12,
+                                  offset: Offset(5, 5),
+                                  blurRadius: 10,
+                                )
+                              ],
+                            ),
+                            child: Center(
+                              child: Text(
+                                'Sign in (ONLY FOR DEBUG)',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: !_loggingIn
+                              ? Text(
+                                  error,
+                                  style: TextStyle(color: Colors.red),
+                                )
+                              : CircularProgressIndicator(),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: SizedBox(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ],
-            ),
-          ),
-        ),
+            )),
       ),
-    ),
     );
   }
 }
