1c1d94185bd40d4ad1da51cc89fb07a80a9f035c
SAMBIT MAJHI <majhisambit2@gmail.com>
2020-03-25 00:42:47 +0530

    update

--- lib/Screens/MerchandisingMang/MerchandisingMang.dart
+++ lib/Screens/MerchandisingMang/MerchandisingMang.dart
@@ -12,118 +12,121 @@ class _MerchandisingMangState extends State<MerchandisingMang> {
         appBar: AppBar(
           title: Text("Merchandising Management"),
         ),
-        body: new GridView.count(
-          
-          crossAxisCount: 2,
-          childAspectRatio: 1.0,
-          padding: const EdgeInsets.all(4.0),
-          mainAxisSpacing: 8.0,
-          crossAxisSpacing: 8.0,
-          children: <Widget>[
-            GestureDetector(
-              child: Card(
-                elevation: 8.0,
-                child: Container(
-                  color: Colors.grey[300],
-                  alignment: Alignment.center,
-                  child: Text('BoM', style: TextStyle(fontSize: 25.0),textAlign: TextAlign.center,),
-                )
+        body: Padding(
+          padding: const  EdgeInsets.fromLTRB(20, 30, 20, 10),
+          child: new GridView.count(
+            
+            crossAxisCount: 2,
+            childAspectRatio: 1.0,
+            padding: const EdgeInsets.all(4.0),
+            mainAxisSpacing: 20.0,
+            crossAxisSpacing: 20.0,
+            children: <Widget>[
+              GestureDetector(
+                child: Card(
+                  elevation: 8.0,
+                  child: Container(
+                    color: Colors.grey[300],
+                    alignment: Alignment.center,
+                    child: Text('BoM', style: TextStyle(fontSize: 22.0),textAlign: TextAlign.center,),
+                  )
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, '/BoM');
+                },
+              ),GestureDetector(
+                child: Card(
+                  elevation: 8.0,
+                  child: Container(
+                    color: Colors.grey[300],
+                    alignment: Alignment.center,
+                    child: Text('TnA', style: TextStyle(fontSize: 22.0),textAlign: TextAlign.center,),
+                  )
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, '/TnA');
+                },
               ),
-              onTap: () {
-                Navigator.pushNamed(context, '/BoM');
-              },
-            ),GestureDetector(
-              child: Card(
-                elevation: 8.0,
-                child: Container(
-                  color: Colors.grey[300],
-                  alignment: Alignment.center,
-                  child: Text('TnA', style: TextStyle(fontSize: 25.0),textAlign: TextAlign.center,),
-                )
+              GestureDetector(
+                child: Card(
+                  elevation: 8.0,
+                  child: Container(
+                    color: Colors.grey[300],
+                    alignment: Alignment.center,
+                    child: Text('Fabric\nEnquiry', style: TextStyle(fontSize: 22.0),textAlign: TextAlign.center,),
+                  )
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, '/FabricEnquiry');
+                },
               ),
-              onTap: () {
-                Navigator.pushNamed(context, '/TnA');
-              },
-            ),
-            GestureDetector(
-              child: Card(
-                elevation: 8.0,
-                child: Container(
-                  color: Colors.grey[300],
-                  alignment: Alignment.center,
-                  child: Text('Fabric\nEnquiry', style: TextStyle(fontSize: 25.0),textAlign: TextAlign.center,),
-                )
+              GestureDetector(
+                child: Card(
+                  elevation: 8.0,
+                  child: Container(
+                    color: Colors.grey[300],
+                    alignment: Alignment.center,
+                    child: Text('Stock\nMangement', style: TextStyle(fontSize: 22.0),textAlign: TextAlign.center,),
+                  )
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, '/StockMang');
+                },
               ),
-              onTap: () {
-                Navigator.pushNamed(context, '/FabricEnquiry');
-              },
-            ),
-            GestureDetector(
-              child: Card(
-                elevation: 8.0,
-                child: Container(
-                  color: Colors.grey[300],
-                  alignment: Alignment.center,
-                  child: Text('Stock\nMangement', style: TextStyle(fontSize: 25.0),textAlign: TextAlign.center,),
-                )
+              GestureDetector(
+                child: Card(
+                  elevation: 8.0,
+                  child: Container(
+                    color: Colors.grey[300],
+                    alignment: Alignment.center,
+                    child: Text('Sample\nTracking', style: TextStyle(fontSize: 22.0),textAlign: TextAlign.center,),
+                  )
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, '/SampleTrack');
+                },
               ),
-              onTap: () {
-                Navigator.pushNamed(context, '/StockMang');
-              },
-            ),
-            GestureDetector(
-              child: Card(
-                elevation: 8.0,
-                child: Container(
-                  color: Colors.grey[300],
-                  alignment: Alignment.center,
-                  child: Text('Sample\nTracking', style: TextStyle(fontSize: 25.0),textAlign: TextAlign.center,),
-                )
+              GestureDetector(
+                child: Card(
+                  elevation: 8.0,
+                  child: Container(
+                    color: Colors.grey[300],
+                    alignment: Alignment.center,
+                    child: Text('Order\nTracking', style: TextStyle(fontSize: 22.0),textAlign: TextAlign.center,),
+                  )
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, '/OrderTrack');
+                },
               ),
-              onTap: () {
-                Navigator.pushNamed(context, '/SampleTrack');
-              },
-            ),
-            GestureDetector(
-              child: Card(
-                elevation: 8.0,
-                child: Container(
-                  color: Colors.grey[300],
-                  alignment: Alignment.center,
-                  child: Text('Order\nTracking', style: TextStyle(fontSize: 25.0),textAlign: TextAlign.center,),
-                )
+              GestureDetector(
+                child: Card(
+                  elevation: 8.0,
+                  child: Container(
+                    color: Colors.grey[300],
+                    alignment: Alignment.center,
+                    child: Text('Supplier\nManagement', style: TextStyle(fontSize: 22.0),textAlign: TextAlign.center,),
+                  )
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, '/SuppliersMang');
+                },
               ),
-              onTap: () {
-                Navigator.pushNamed(context, '/OrderTrack');
-              },
-            ),
-            GestureDetector(
-              child: Card(
-                elevation: 8.0,
-                child: Container(
-                  color: Colors.grey[300],
-                  alignment: Alignment.center,
-                  child: Text('Supplier\nManagement', style: TextStyle(fontSize: 25.0),textAlign: TextAlign.center,),
-                )
+              GestureDetector(
+                child: Card(
+                  elevation: 8.0,
+                  child: Container(
+                    color: Colors.grey[300],
+                    alignment: Alignment.center,
+                    child: Text('Buyer\'s\nManagement', style: TextStyle(fontSize: 22.0),textAlign: TextAlign.center,),
+                  )
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, '/BuyersMang');
+                },
               ),
-              onTap: () {
-                Navigator.pushNamed(context, '/SuppliersMang');
-              },
-            ),
-            GestureDetector(
-              child: Card(
-                elevation: 8.0,
-                child: Container(
-                  color: Colors.grey[300],
-                  alignment: Alignment.center,
-                  child: Text('Buyer\'s\nManagement', style: TextStyle(fontSize: 25.0),textAlign: TextAlign.center,),
-                )
-              ),
-              onTap: () {
-                Navigator.pushNamed(context, '/BuyersMang');
-              },
-            ),
-          ],
+            ],
+          ),
         ));
   }
 }
