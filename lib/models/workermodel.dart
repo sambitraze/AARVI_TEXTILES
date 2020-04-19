104a78027fccc9e68cb09de12de978c837ba4d01
SAMBIT MAJHI <majhisambit2@gmail.com>
2020-04-19 14:22:05 +0530

    update class

--- lib/models/workermodel.dart
+++ lib/models/workermodel.dart
@@ -2,12 +2,13 @@ class Worker {
 
   final date;
   final uid;
+  final password;
   final name;
   final sam;
   final lineno;
   final timein;
   final timeout;
 
-  Worker({this.date,this.lineno,this.name,this.sam,this.timein,this.timeout,this.uid});
+  Worker({this.date,this.lineno,this.name,this.sam,this.timein,this.timeout,this.uid,this.password});
 
 }
