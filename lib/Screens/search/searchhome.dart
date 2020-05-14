9be1d93dcfe061bebcb706d90e1f050c7ddc1203
rohan <rkakar2000@gmail.com>
2020-05-14 12:27:35 +0530

    Search Page 1

--- lib/Screens/search/searchhome.dart
+++ lib/Screens/search/searchhome.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class SearchHome extends StatefulWidget {
@@ -6,7 +7,16 @@ class SearchHome extends StatefulWidget {
 }
 
 class _SearchHomeState extends State<SearchHome> {
-  String styleno;
+  String styleno = '';
+  List items = [
+    'Cutting Quality',
+    'Daily Cutting Report',
+    'Operation Bulletin',
+    'Daily Sewing Report',
+    'Packing',
+    'Time Study'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,9 +27,10 @@ class _SearchHomeState extends State<SearchHome> {
             Container(
               padding: EdgeInsets.all(10.0),
               child: TextFormField(
+                onChanged: (v) => styleno = v,
                 decoration: InputDecoration(
                   prefixIcon:
-                      Icon(Icons.search, color: Colors.black, size: 30.0),
+                  Icon(Icons.search, color: Colors.black, size: 30.0),
                   prefix: Text('Style No: '),
                   filled: true,
                   fillColor: Colors.white,
@@ -39,7 +50,11 @@ class _SearchHomeState extends State<SearchHome> {
               child: MaterialButton(
                 elevation: 8,
                 padding: EdgeInsets.all(10.0),
-                onPressed: () => print('asfs'),
+                onPressed: () {
+                  setState(() {
+
+                  });
+                },
                 child: Text(
                   'Fetch',
                   style: TextStyle(
@@ -52,8 +67,50 @@ class _SearchHomeState extends State<SearchHome> {
                     borderRadius: BorderRadius.circular(20)),
               ),
             ),
-            Container(
-              //TODO: to show basic details such as adress , buyer, etc
+            Expanded(
+              child: StreamBuilder(
+                stream: styleno != ''
+                    ? Firestore.instance.collection('aarvi').where(
+                    'style', isEqualTo: styleno).snapshots()
+                    : Firestore.instance.collection('aarvi').snapshots()
+                ,
+                builder: (context, snapshot) {
+                  print("Starting render");
+                  if (snapshot.hasData) {
+                    List newList = snapshot.data.documents.map<Widget>((e) =>
+                        ExpansionTile(
+                            key: Key(e['style']),
+                            title: Text(e['name'] ?? '',
+                              style: TextStyle(fontWeight: FontWeight.bold),),
+                            subtitle: Text.rich(
+                                TextSpan(
+                                    text: "Order No:",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w600),
+                                    children: <TextSpan>[
+                                      TextSpan(text: e['style'],
+                                          style: DefaultTextStyle
+                                              .of(context)
+                                              .style)
+                                    ]
+                                )
+                            ),
+                            children: items.map<Widget>((e) =>
+                                ListTile(
+                                  title: Text(e),
+                                )).toList()
+                        )).toList();
+                    print("List created");
+                    print(newList.runtimeType);
+                    return ListView(
+                        children: newList
+                    );
+                  }
+                  else {
+                    return Text("No data");
+                  }
+                },
+              ),
             )
           ],
         ),
